8728e862dd32147b6cfe7e6c505a27867e8534c4
natalymr <nataliamurycheva94@gmail.com>
2018-04-13 05:47:21 +0300

    Add a solution

---
+++ main.py
@@ -0,0 +1,53 @@
+#!/bin/python
+from io import StringIO
+
+from commands import *
+
+envs = dict()
+
+
+def loop():
+    """Main interpreter loop"""
+    envs = dict()
+    while True:
+        line = input(">")
+        try:
+            process(line, envs)
+        except Exception as e:
+            print(e)
+
+
+def process(line, envs):
+    """Processes each incoming of interpreter
+    line: incoming line
+    envs: dict of environment variables
+    """
+    line = substitute_dollar(line, envs)
+    blocks = split_by_pipeline(line)
+
+    stream2 = StringIO("")
+    for i, block in enumerate(blocks):
+
+        assig = is_assignment(block)
+        if assig:
+            envs[assig[0]] = assig[1]
+            continue
+
+        cmd, args = block_to_cmd_args(block)
+        cls = multiplexor(cmd)
+        if cls:
+            stream1 = stream2
+            stream2 = StringIO("")
+            execer = cls(InputStream=stream1, OutputStream=stream2,
+                         env=envs)
+            execer.exec(args)
+            stream2.seek(0)
+
+    print(stream2.getvalue())
+
+
+# test_s = "'echo' $ \"a|$sdf$a$a  $m\" | cat aa '$asd|fgf'$h | echo $abcde'a$\"sd\"f' | pwd 'asdfasd' | cat example.txt"
+# test_s = "echo 3 | cat | ls|echo $a|pwd | cat | cat |cat|cat"
+# test_s = 'echo 123 | wc'
+if __name__ == "__main__":
+    loop()
---
+++ test.py
@@ -0,0 +1,145 @@
+import unittest
+from io import StringIO
+
+from parser import *
+from commands import *
+
+
+class ParserTestCase(unittest.TestCase):
+    def test_clip_both_quotes(self):
+        s1 = "'asdfsf\"asd\' \"jks jf\"ks\' '"
+        ref1 = ([(0, 11), (11, 23), (23, 26)], True,
+                [(0, 13), (13, 20), (20, 26)], False,
+                [(0, 11), (11, 13), (13, 20), (20, 23), (23, 26)], True)
+
+        s2 = "\"'\" '\"'  '\" \"'  \" ' '\""
+        ref2 = ([(0, 4), (4, 6), (6, 9), (9, 13), (13, 22)], False,
+                [(0, 2), (2, 16), (16, 22)], True,
+                [(0, 2), (2, 4), (4, 6), (6, 9), (9, 13), (13, 16), (16, 22)],
+                True)
+
+        assert clip_both_quotes(s1) == ref1
+        assert clip_both_quotes(s2) == ref2
+
+    def test_strip_quotes(self):
+        s1 = "'asdfsf\"asd\' \"jks jf\"ks\' '"
+        ref1 = "asdfsf\"asd jks jfks "
+        s2 = "\"'\""
+        ref2 = "'"
+
+        assert strip_quotes(s1) == ref1
+        assert strip_quotes(s2) == ref2
+
+    def test_split_by_spaces(self):
+        s1 = "aaaa 'aaa aa 'aaa b\"bb\""
+        ref1 = ['aaaa', "'aaa aa 'aaa", 'b"bb"']
+        assert split_by_spaces(s1) == ref1
+
+    def test_substitute_dollar(self):
+        env = {"a": 'A',
+               "b": 'B',
+               "c": 'C'}
+        s1 = "$abc-$a.bs'$b'$c'$____' $ $c $"
+        ref1 = "-A.bs'$b'C'$____'  C "
+        assert substitute_dollar(s1, env) == ref1
+
+    def test_split_by_pipeline(s):
+        s1 = "'echo' $ \"a|$sdf$a$a  $m\" | cat aa '$asd|fgf'$h | echo"
+        assert split_by_pipeline(s1) == ['\'echo\' $ "a|$sdf$a$a  $m" ',
+                                         " cat aa '$asd|fgf'$h ", ' echo']
+
+    def test_block_to_cmd_args(self):
+        s1 = "'ec'\"ho\" 123 $a"
+        ref1 = ('echo', ' 123 $a')
+        assert block_to_cmd_args(s1) == ref1
+
+    def test_is_assigment(self):
+        s1 = "jasldj \"=b\"=2"
+        ref1 = False
+        assert is_assignment(s1) == ref1
+
+        s2 = "jasldj\"=b\"=2"
+        ref2 = False
+        assert is_assignment(s2) == ref2
+
+        s3 = "mmm=2"
+        ref3 = ["mmm", '2']
+        assert is_assignment(s3) == ref3
+
+
+class CommandsTestCase(unittest.TestCase):
+    def setUp(self):
+        self.env = {"a": 'A',
+                    "b": 'B',
+                    "c": 'C',
+                    "e": 'ex',
+                    "i": "it"}
+
+    def test_multiplexor(self):
+        s1 = "ls"
+        s2 = "echo"
+        s3 = "cat"
+        s4 = "exit"
+        s5 = "pwd"
+
+        assert multiplexor(s1) == Default
+        assert multiplexor(s2) == Echo
+        assert multiplexor(s3) == Cat
+        assert multiplexor(s4) == Exit
+        assert multiplexor(s5) == Pwd
+
+    def test_Default(self):
+        args = "example.txt"
+
+        stream1 = StringIO()
+        stream2 = StringIO()
+        d = Default(stream1, stream2)
+
+        d.cmd = "ls"
+        d.exec(args)
+        ref1 = "example.txt\n\n"
+
+        assert stream2.getvalue() == ref1
+
+    def test_Echo(self):
+        args = "example.txt"
+
+        stream1 = StringIO()
+        stream2 = StringIO()
+        d = Echo(stream1, stream2)
+
+        d.exec(args)
+        ref1 = "example.txt\n"
+
+        assert stream2.getvalue() == ref1
+
+    def test_Cat(self):
+        args = "example.txt "
+
+        stream1 = StringIO()
+        stream2 = StringIO()
+        d = Cat(stream1, stream2)
+
+        d.exec(args)
+        ref1 = "lsdjf" \
+               "\njsldfja" \
+               "\nf dfhjasdf " \
+               "\nasdfj"
+
+        assert stream2.getvalue() == ref1
+
+    def test_Wc(self):
+        args = "example.txt "
+
+        stream1 = StringIO()
+        stream2 = StringIO()
+        d = Wc(stream1, stream2)
+
+        d.exec(args)
+        ref1 = '(4, 5, 31)\n'
+
+        assert stream2.getvalue() == ref1
+
+
+if __name__ == '__main__':
+    unittest.main()
